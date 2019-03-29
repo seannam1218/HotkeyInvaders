ff3c86061c82b2880454bd65fa06f6b065b49154
Sean Nam <seannam1218@gmail.com>
2019-03-28 22:27:03 -0700

    initial commit

---
+++ Player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+var mouse_position = Vector2() #+ camera.global_position
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	mouse_position = get_viewport().get_mouse_position()
+	position.x = mouse_position.x
+
+
+
+
+
+
+
+#<div>Icons made by <a href="https://www.flaticon.com/authors/bqlqn" title="bqlqn">bqlqn</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
